aa9d769e5b403b817d333846e065a693bea39fa8
Mahla <Mahlakhalili1@gmail.com>
2024-08-23 01:15:44 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from "react"
+import Header from './components/Header';
 
 function App() {
-
-
-  return (
-    <>
-    </>
-  )
+	return (
+		<div className="app">
+			<Header />
+		</div>
+	);
 }
 
-export default App
+export default App;
