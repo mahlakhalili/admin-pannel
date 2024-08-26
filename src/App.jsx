987ca177ac2a8792ef8a93d89ed94da2ae4c4ac3
Mahla <Mahlakhalili1@gmail.com>
2024-08-26 01:14:21 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
 	return (
-		<div className="app">
+		<div className="app grid grid-cols-[max-content_1fr] grid-rows-[max-content_1fr] min-h-screen">
 			<Header />
+			<Sidebar />
+			<main></main>
 		</div>
 	);
 }
