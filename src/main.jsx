62d575b331ce72da4f7f67c64eab04aff3974bfe
Mahla <Mahlakhalili1@gmail.com>
2024-08-27 02:54:12 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Router from "./Router.jsx";
-import { RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Router from './Router.jsx';
+import { RouterProvider } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={Router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  </StrictMode>
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={Router} />
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	</StrictMode>
 );
