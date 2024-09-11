73f0152511ed502cfb4e63a2106e2b863ba36785
Mahla <Mahlakhalili1@gmail.com>
2024-09-11 03:19:00 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="app grid grid-cols-[max-content_1fr] grid-rows-[max-content_1fr] min-h-screen">
+		<div className="app grid grid-rows-[max-content_1fr] grid-cols-[max-content_1fr]  min-h-screen">
 			<Header />
 			<Sidebar />
-			<main></main>
+			<main className='p-4'>
+				<Outlet />
+			</main>
 		</div>
 	);
 }
