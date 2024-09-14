914115b6b66e4e1bb29676a6b266aa636bb821b4
Mahla <Mahlakhalili1@gmail.com>
2024-09-14 06:05:37 -0700

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
-
+import ProductList from './pages/ProductList';
+import AddProduct from './pages/AddProduct';
 const Router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
+		children: [
+			{
+				path: '/product/list',
+				element: <ProductList />,
+			},
+			{
+				path: '/product/add',
+				element: <AddProduct />,
+			},
+		],
 	},
 ]);
 
