620bf06bb2d932f9ef978149092bb03df7132782
Mahla <Mahlakhalili1@gmail.com>
2024-09-16 01:35:03 -0700

    Update ProductForm.jsx

--- src/components/product/ProductForm.jsx
+++ src/components/product/ProductForm.jsx
@@ -1,45 +1,99 @@
 import { useForm } from 'react-hook-form';
+import { Form } from 'react-router-dom';
 
 const ProductForm = () => {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			firstName: 'مهلا',
+			lastName: 'خلیلی',
+			age: '26',
+			job: 'دانشجو',
+		},
+	});
 
 	return (
-		<div className="grid gap-4">
+		<Form
+			className="grid gap-4"
+			onSubmit={handleSubmit(
+				(data) => {
+					console.log(data);
+				},
+				(error) => {
+					console.log(error);
+				}
+			)}
+		>
 			<div className="form-input">
 				<label>نام</label>
 				<input
 					type="text"
-					placeholder="John"
-					{...register('firstName')}
+					{...register('firstName', {
+						required: true,
+						minLength: 5,
+						maxLength: 20,
+					})}
 				/>
+				{errors?.firstName?.type === 'required' && (
+					<span className="text-red-600 text-sm">نام خود را وارد کنید</span>
+				)}
+				{errors?.firstName?.type === 'minLength' && (
+					<span className="text-red-600 text-sm">نام باید بیش از پنج حرف باشد</span>
+				)}
+				{errors?.firstName?.type === 'maxLength' && (
+					<span className="text-red-600 text-sm">نام باید کمتر از بیست حرف باشد</span>
+				)}
 			</div>
 			<div className="form-input">
 				<label>نام خانوادگی</label>
 				<input
 					type="text"
-					placeholder="John"
-					{...register('lastName')}
+					{...register('lastName', {
+						required: true,
+					})}
 				/>
+			</div>
+			<div className="form-input">
+				<label>سن</label>
+				<input
+					name="age"
+					type="number"
+					{...register('age', {
+						required: true,
+						min: 18,
+						max: 99,
+					})}
+				/>
+				{errors?.age?.type === 'required' && (
+					<span className="text-red-600 text-sm">سن خود را وارد کنید</span>
+				)}
+				{errors?.age?.type === 'min' && (
+					<span className="text-red-600 text-sm"> سن شما باید بیش از 18 سال باشد</span>
+				)}
+				{errors?.age?.type === 'max' && (
+					<span className="text-red-600 text-sm"> سن شما باید کمتر از 99 سال باشد</span>
+				)}
 			</div>
 			<div className="form-input">
 				<label>شغل</label>
 				<input
 					name="job"
 					type="text"
-					placeholder="John"
-					{...register('job')}
+					{...register('job', {
+						required: true,
+					})}
 				/>
 			</div>
 			<button
-				type="button"
-				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none "
-				onClick={handleSubmit((data) => {
-					console.log(data);
-				})}
+				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none"
+				type="submit"
 			>
 				Default
 			</button>
-		</div>
+		</Form>
 	);
 };
 
