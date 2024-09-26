a1e772ce9dd62cd67d2c6eb22dbeab076255ebdf
Mahla <Mahlakhalili1@gmail.com>
2024-09-26 01:59:56 -0700

    Update ProductForm.jsx

--- src/components/product/ProductForm.jsx
+++ src/components/product/ProductForm.jsx
@@ -1,60 +1,73 @@
-import { useForm } from 'react-hook-form';
-import { Form } from 'react-router-dom';
+import { useForm, FormProvider } from 'react-hook-form';
 import Input from '../Input';
+import { Form } from 'react-hook-form';
 
 const ProductForm = () => {
-	const {
-		register,
-		handleSubmit,
-		// formState: { errors },
-		getValues,
-		setValue,
-		watch,
-		// reset,
-		setError,
-		// setFocus
-	} = useForm({
+	
+	const formMethods = useForm({
 		defaultValues: {
-			firstName: '',
-			lastName: '',
-			age: '',
-			job: '',
-			gender: 'female',
-			maritalStatus: 'singel',
-			isAbove18: 'above18',
-			showGender: true,
+			firstName: 'مهلا',
+			lastName: 'خلیلی',
 		},
 	});
+	
 
 	return (
-		<Form
-			className="grid gap-4"
-			onSubmit={handleSubmit(
-				(data) => {
-					console.log(data);
-				},
-				(error) => {
-					console.log(error);
-				}
-			)}
-		>
-			<Input
-				label={'نام'}
-				type="text"
-			/>
-			<Input
-				label={'نام خانوادگی'}
-				type="text"
-			/>
-			<Input
-				label={'سن'}
-				type="number"
-			/>
-			<Input
-				label={'شغل'}
-				type="text"
-			/>
-			{/* <div className="form-input">
+		<FormProvider {...formMethods}>
+			<Form
+				className="grid gap-4"
+				onSubmit={formMethods.handleSubmit(
+					(data) => {
+						console.log(data);
+					},
+					(error) => {
+						console.log(error);
+					}
+				)}
+			>
+				{/* <Controller
+					control={formMethods.control}
+					name="ReactDatepicker"
+					render={({ field: { onChange, onBlur, value, ref } }) => (
+						
+					)}
+					/> */}
+				<Input
+					label={'نام'}
+					type="text"
+					name="firstName"
+					rules={{
+						required: 'نام خود را وارد کنید',
+						minLength: {
+							value: 2,
+							message: 'نام باید بیش از دو حرف باشد',
+						},
+						maxLength: {
+							value: 20,
+							message: 'نام باید کمتر از بیست حرف باشد',
+						},
+					}}
+				/>
+				<Input
+					label={'نام خانوادگی'}
+					type="text"
+					name="lastName"
+				/>
+
+				{/* 
+				<Input
+					label={'نام خانوادگی'}
+					type="text"
+				/>
+				<Input
+					label={'سن'}
+					type="number"
+				/>
+				<Input
+					label={'شغل'}
+					type="text"
+				/> */}
+				{/* <div className="form-input">
 				<label>نام</label>
 				<input
 					type="text"
@@ -74,7 +87,7 @@ const ProductForm = () => {
 					<span className="text-red-600 text-sm">نام باید کمتر از بیست حرف باشد</span>
 				)}
 			</div> */}
-			{/* <div className="form-input">
+				{/* <div className="form-input">
 				<label>نام خانوادگی</label>
 				<input
 					type="text"
@@ -93,16 +106,16 @@ const ProductForm = () => {
 					</select>
 				</div>
 			</div> */}
-			<div className=" flex items-center mb-4">
-				<input
-					type="checkbox"
-					value="showGender"
-					className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-					{...register('showGender')}
-				/>
-				<label className="ms-2 text-sm font-medium text-gray-900 ">نمایش جنسیت</label>
-			</div>
-			{watch('showGender') && (
+				{/* <div className=" flex items-center mb-4">
+					<input
+						type="checkbox"
+						value="showGender"
+						className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
+						// {...register('showGender')}
+					/>
+					<label className="ms-2 text-sm font-medium text-gray-900 ">نمایش جنسیت</label>
+				</div> */}
+				{/* {watch('showGender') && (
 				<>
 					<div className=" flex items-center mb-4">
 						<input
@@ -123,9 +136,9 @@ const ProductForm = () => {
 						<label className="ms-2 text-sm font-medium text-gray-900 ">مرد</label>
 					</div>
 				</>
-			)}
+			)} */}
 
-			{/* <div className="flex items-center mb-4">
+				{/* <div className="flex items-center mb-4">
 				<input
 					id="default-radio-1"
 					type="radio"
@@ -149,7 +162,7 @@ const ProductForm = () => {
 					پایین 18 سال
 				</label>
 			</div> */}
-			{/* <div className="form-input">
+				{/* <div className="form-input">
 				<label>سن</label>
 				<input
 					name="age"
@@ -170,7 +183,7 @@ const ProductForm = () => {
 					<span className="text-red-600 text-sm"> سن شما باید کمتر از 99 سال باشد</span>
 				)}
 			</div> */}
-			{/* <div className="form-input">
+				{/* <div className="form-input">
 				<label>شغل</label>
 				<input
 					name="job"
@@ -181,29 +194,30 @@ const ProductForm = () => {
 				/>
 			</div> */}
 
-			<button
-				className="text-white text-lg  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 focus:outline-none"
-				type="submit"
-			>
-				ارسال
-			</button>
-			<button
-				type="button"
-				onClick={() => {
-					console.log(getValues('firstName'));
-					console.log(watch('lastName'));
-					setValue('firstName', 'mahla khalili');
-					// reset()
-					// setFocus('firstName')
-					setError('lastName', {
-						type: 'server',
-						message: 'error',
-					});
-				}}
-			>
-				ذخیره
-			</button>
-		</Form>
+				<button
+					className="text-white text-lg  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 focus:outline-none"
+					type="submit"
+				>
+					ارسال
+				</button>
+				{/* <button
+					type="button"
+					// onClick={() => {
+					// 	// console.log(getValues('firstName'));
+					// 	// console.log(watch('lastName'));
+					// 	// setValue('firstName', 'mahla khalili');
+					// 	// reset()
+					// 	// setFocus('firstName')
+					// 	// setError('lastName', {
+					// 		// type: 'server',
+					// 		// message: 'error',
+					// 	});
+					// }}
+				>
+					ذخیره
+				</button> */}
+			</Form>
+		</FormProvider>
 	);
 };
 
