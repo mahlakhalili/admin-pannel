bb96a4e304b7904c12f7ae319d2777c94918e6f0
Mahla <Mahlakhalili1@gmail.com>
2024-09-26 01:59:40 -0700

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,42 @@
 import PropTypes from 'prop-types';
-const Input = ({ label, type = 'text', placeholder = '' }) => {
+import { useController, useFormContext } from 'react-hook-form';
+const Input = ({
+	label,
+	type = 'text',
+	placeholder = '',
+	name = '',
+	defaultValue = '',
+	rules={},
+}) => {
+	const { control } = useFormContext();
+	const { field, fieldState } = useController({
+		control,
+		defaultValue,
+		name,
+		rules
+	});
 	return (
 		<div className="form-input">
 			<label>{label}</label>
 			<input
+				{...field}
 				type={type}
 				placeholder={placeholder}
 			/>
+
+			{fieldState?.error && (
+				<span className="text-red-600 text-sm">{fieldState.error.message}</span>
+			)}
 		</div>
 	);
 };
 
 Input.propTypes = {
 	label: PropTypes.string,
-	type: PropTypes.oneOf(['text' , 'email' , 'password' , 'number' ]),
+	type: PropTypes.oneOf(['text', 'email', 'password', 'number']),
 	placeholder: PropTypes.string,
+	name: PropTypes.string.isRequired,
+	defaultValue: PropTypes.string,
+	rules: PropTypes.object,
 };
 export default Input;
