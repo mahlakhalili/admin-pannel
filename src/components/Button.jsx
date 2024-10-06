f9f24433610b733950ed4dba360884b97d107df0
Mahla <Mahlakhalili1@gmail.com>
2024-10-06 03:04:03 -0700

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
-const Button = ({ text, onClick }) => {
+const Button = ({ text, onClick, type = 'button' }) => {
 	return <button onClick={onClick}>{text}</button>;
 };
 Button.propTypes = {
 	text: PropTypes.string,
 	onClick: PropTypes.func,
+	type: PropTypes.oneOf(['button', 'submit']),
 };
 export default Button;
