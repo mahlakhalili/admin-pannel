67f195109a0c1ddf357a58b37c6d19981bc459a1
Mahla <Mahlakhalili1@gmail.com>
2024-10-07 01:01:54 -0700

    Update AddProduct.jsx

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -2,7 +2,7 @@ import ProductForm from '../components/product/ProductForm';
 const AddProduct = () => {
 	return (
 		<div className='grid gap-4 font-normal'>
-      <h1 className='text-2xl'>افزودن کاربر</h1>
+      <h1 className='text-2xl'>افزودن محصول</h1>
 			<ProductForm />
 		</div>
 	);
