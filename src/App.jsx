a728972eb3bf27c683dbf104dee2551da7cf9431
Mahla <Mahlakhalili1@gmail.com>
2024-10-09 01:33:03 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 	return (
 		<div className="app grid grid-rows-[max-content_1fr] grid-cols-[max-content_1fr]  min-h-screen">
 			<Header />
 			<Sidebar />
-			<main className='p-4'>
+			<main className="p-4">
 				<Outlet />
 			</main>
+			<ToastContainer
+				position="top-right"
+				theme="colored"
+				rtl
+			/>
 		</div>
 	);
 }
