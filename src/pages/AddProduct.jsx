16f72b8dbd2e6308417f167942468f5a7372dc38
Mahla <Mahlakhalili1@gmail.com>
2024-10-15 00:36:19 -0700

    Update AddProduct.jsx

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,9 +1,16 @@
 import ProductForm from '../components/product/ProductForm';
+import useAddProduct from '../hooks/apis/product/useAddProduct';
 const AddProduct = () => {
+	const addProduct = useAddProduct();
+
 	return (
-		<div className='grid gap-4 font-normal'>
-      <h1 className='text-2xl'>افزودن محصول</h1>
-			<ProductForm />
+		<div className="grid gap-4 font-normal">
+			<h1 className="text-2xl">افزودن محصول</h1>
+			<ProductForm
+				onAdd={(formData) => {
+					addProduct.mutate(formData);
+				}}
+			/>
 		</div>
 	);
 };
