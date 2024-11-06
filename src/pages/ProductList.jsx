bc32407cdec561481d0a763d679a24d8fe67a498
Mahla <Mahlakhalili1@gmail.com>
2024-11-06 00:09:16 -0800

    Update ProductList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,11 +1,24 @@
-
-
 const ProductList = () => {
-  return (
-    <div>
-      product 
-    </div>
-  )
-}
+	return (
+		<div className="page">
+			<h1 className="text-2xl">لیست محصولات</h1>
 
-export default ProductList
+			<div className="table-wrapper">
+				<table className="table-wrapper table">
+					<thead className="table-wrapper table thead">
+						<tr>
+							<th>ردیف</th>
+							<th>عنوان محصول</th>
+							<th>قیمت</th>
+							<th>تخفیف</th>
+							<th>تعداد</th>
+						</tr>
+					</thead>
+					<tbody></tbody>
+				</table>
+			</div>
+		</div>
+	);
+};
+
+export default ProductList;
