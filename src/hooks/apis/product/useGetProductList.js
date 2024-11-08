399dd0d394c3d558d5b268a582a2cb6751cf3cc2
Mahla <Mahlakhalili1@gmail.com>
2024-11-08 09:12:45 -0800

    Update useGetProductList.js

--- src/hooks/apis/product/useGetProductList.js
+++ src/hooks/apis/product/useGetProductList.js
@@ -1,3 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
+const useGetProductList = () => {
+	return useQuery({
+        queryKey:[],
+		queryFn: (data) =>
+			axios.get('http://localhost:1000/product', data).then((res) => res.data),
+	});
+};
+
+export default useGetProductList;
