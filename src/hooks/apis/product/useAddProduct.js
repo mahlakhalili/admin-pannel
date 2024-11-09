f6cec28d2fa61629ecd709d8fabb2a88e364d2a6
Mahla <Mahlakhalili1@gmail.com>
2024-11-09 00:40:05 -0800

    Update useAddProduct.js

--- src/hooks/apis/product/useAddProduct.js
+++ src/hooks/apis/product/useAddProduct.js
@@ -1,12 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const useAddProduct = () => {
+const useAddProduct = ({ onAdd = () => {} }) => {
+	const queryClient = useQueryClient();
 	return useMutation({
 		mutationFn: (data) =>
 			axios.post('http://localhost:1000/product', data).then((res) => res.data),
 		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['products'],
+			});
+			onAdd();
 			toast.success('محصول با موفقیت افزوده شد');
 		},
 	});
