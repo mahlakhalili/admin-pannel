e2dda1ad2c5ab21a27cc814f6e267b33ee8bc9ec
Mahla <Mahlakhalili1@gmail.com>
2024-11-09 23:35:10 -0800

    Update ProductList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,9 @@
 import useGetProductList from '../hooks/apis/product/useGetProductList';
+import useDeleteProduct from '../hooks/apis/product/useDeleteProduct';
+import Button from '../components/Button';
 const ProductList = () => {
 	const { data: products } = useGetProductList();
+	const deleteProduct = useDeleteProduct();
 	return (
 		<div className="page">
 			<h1 className="text-2xl">لیست محصولات</h1>
@@ -14,6 +17,7 @@ const ProductList = () => {
 							<th>قیمت</th>
 							<th>تخفیف</th>
 							<th>تعداد</th>
+							<th className="row">عملیات</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -24,6 +28,15 @@ const ProductList = () => {
 								<td>{product.price}</td>
 								<td>{product.discount || 0}</td>
 								<td>{product.count}</td>
+								<td>
+									<Button
+										text="حذف"
+										color="red"
+										onClick={() => {
+											deleteProduct.mutate(product.id);
+										}}
+									/>
+								</td>
 							</tr>
 						))}
 					</tbody>
