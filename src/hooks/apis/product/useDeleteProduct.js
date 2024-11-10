2e1f663b5bc2c9da4cb07fd6f1316923a051723f
Mahla <Mahlakhalili1@gmail.com>
2024-11-10 00:21:54 -0800

    Update useDeleteProduct.js

--- src/hooks/apis/product/useDeleteProduct.js
+++ src/hooks/apis/product/useDeleteProduct.js
@@ -1,12 +1,16 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const useDeleteProduct = () => {
+	const queryClient = useQueryClient();
 	return useMutation({
 		mutationFn: (id) =>
 			axios.delete(`http://localhost:1000/product/${id}`).then((res) => res.data),
 		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['products'],
+			});
 			toast.success('محصول با موفقیت حذف شد');
 		},
 	});
