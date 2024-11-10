ccbbd9f099deb749cf1562683eac1942cf1f4a63
Mahla <Mahlakhalili1@gmail.com>
2024-11-09 23:34:08 -0800

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-const Button = ({ text, onClick, type = 'button' }) => {
+const Button = ({ text, onClick, color = '', type = 'button' }) => {
 	return (
 		<button
 			type={type}
 			onClick={onClick}
-            className='btn'
+			className={`btn ${color}`}
 		>
 			{text}
 		</button>
@@ -13,6 +13,7 @@ const Button = ({ text, onClick, type = 'button' }) => {
 Button.propTypes = {
 	text: PropTypes.string,
 	onClick: PropTypes.func,
+	color: PropTypes.oneOf(['blue', 'green', 'red']),
 	type: PropTypes.oneOf(['button', 'submit']),
 };
 export default Button;
