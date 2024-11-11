c5dc3a92d7cfa564bc4731b863ed3aa98c84a6ea
Mahla <Mahlakhalili1@gmail.com>
2024-11-11 00:06:24 -0800

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { BiLoaderAlt } from 'react-icons/bi';
 const Button = ({
 	text,
@@ -6,14 +7,18 @@ const Button = ({
 	color = '',
 	type = 'button',
 	loading = false,
-	disable = false,
+	disabled = false,
 }) => {
 	return (
 		<button
 			type={type}
 			onClick={onClick}
-			disabled={loading || disable}
-			className={`btn ${color}`}
+			disabled={loading || disabled}
+			className={classNames({
+				btn: true,
+				[color]: color,
+				disabled: loading || disabled,
+			})}
 		>
 			{loading ? <BiLoaderAlt className="animate-spin" /> : text}
 		</button>
@@ -23,7 +28,7 @@ Button.propTypes = {
 	text: PropTypes.string,
 	onClick: PropTypes.func,
 	loading: PropTypes.bool,
-	disable: PropTypes.bool,
+	disabled: PropTypes.bool,
 	color: PropTypes.oneOf(['blue', 'green', 'red']),
 	type: PropTypes.oneOf(['button', 'submit']),
 };
