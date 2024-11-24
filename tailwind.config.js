f5115a6ad21662350729b4ad75ee45988deef317
Mahla <Mahlakhalili1@gmail.com>
2024-11-24 12:05:54 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			keyframes: {
+				'0%': {
+					transform: 'translate3d(100% , 0, 0)',
+				},
+				'100%': {
+					transform: 'translate3d(-100% , 0, 0)',
+				},
+			},
 			animation: {
 				progress: 'progress 1s ease-in-out infinite',
 			},
