bcb4da7cbeb51fca6fb4f97c4f684f27c98bcfa8
Mahla <Mahlakhalili1@gmail.com>
2024-12-07 03:59:06 -0800

    Update EditProduct.jsx

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -1,5 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import ProductForm from '../components/product/ProductForm';
+import useAddProduct from '../hooks/apis/product/useAddProduct';
 const EditProduct = () => {
-	return <div></div>;
+	const navigate = useNavigate();
+	const addProduct = useAddProduct({
+		onAdd: () => navigate('/product/list'),
+	});
+
+	return (
+		<div className="grid gap-4 font-normal">
+			<h1 className="text-2xl">ویرایش محصول</h1>
+			<ProductForm
+				isAdding={addProduct.isPending}
+				onAdd={(formData) => {
+					addProduct.mutate(formData);
+				}}
+			/>
+		</div>
+	);
 };
 
 export default EditProduct;
+
