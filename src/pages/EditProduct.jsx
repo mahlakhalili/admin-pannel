435d2271dfa9abce3b12965d57795d5e06236ced
Mahla <Mahlakhalili1@gmail.com>
2024-12-10 05:27:43 -0800

    Update EditProduct.jsx

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -1,10 +1,18 @@
 import ProductForm from '../components/product/ProductForm';
+import { useNavigate } from 'react-router-dom';
+
 import useGetProduct from '../hooks/apis/product/useGetProduct';
+import useEditProduct from '../hooks/apis/product/useEditProduct';
 import { useParams } from 'react-router-dom';
 
 const EditProduct = () => {
 	const { productId } = useParams();
 	const { data: product, isLoading } = useGetProduct(productId);
+	const navigate = useNavigate();
+
+	const editProduct = useEditProduct({
+		onEdit: () => navigate('/product/list'),
+	});
 	if (isLoading) return <div> کمی صبر کنید ...</div>;
 	return (
 		<div className="grid gap-4 font-normal">
@@ -12,6 +20,10 @@ const EditProduct = () => {
 			<ProductForm
 				mode="EDIT"
 				data={product}
+				isAdding={editProduct.isPending}
+				onAdd={(formData) => {
+					editProduct.mutate(formData);
+				}}
 			/>
 		</div>
 	);
