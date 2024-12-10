50692d19421fe7dc8f9142c16e4cc7a32a7950d7
Mahla <Mahlakhalili1@gmail.com>
2024-12-10 07:51:19 -0800

    Update FormSkeleton.jsx

--- src/skeleton/FormSkeleton.jsx
+++ src/skeleton/FormSkeleton.jsx
@@ -1,5 +1,24 @@
 const FormSkeleton = () => {
-	return <div></div>;
+	return (
+		<div className="page">
+			<div className="skeleton w-40 h-8"></div>
+			<div className="grid gap-4">
+				<InputSkeleton />
+				<InputSkeleton />
+				<InputSkeleton />
+				<InputSkeleton />
+			</div>
+		</div>
+	);
+};
+
+const InputSkeleton = () => {
+	return (
+		<div className="grid gap-2">
+			<div className="skeleton h-3 w-10"></div>
+			<div className="skeleton h-6"></div>
+		</div>
+	);
 };
 
 export default FormSkeleton;
