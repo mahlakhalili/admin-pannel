e02275625c2f1eec01a0be8978bccdc6e11ad8f5
Mahla <Mahlakhalili1@gmail.com>
2024-12-10 04:51:56 -0800

    Update useEditProduct.js

--- src/hooks/apis/product/useEditProduct.js
+++ src/hooks/apis/product/useEditProduct.js
@@ -3,10 +3,16 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const useEditProduct = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        
-    })
-
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: (data) =>
+			axios.patch(`http://localhost:1000/product/${data.id}`).then((res) => res.data),
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['products'],
+			});
+			toast.success('محصول با موفقیت ویرایش شد');
+		},
+	});
 };
 export default useEditProduct;
