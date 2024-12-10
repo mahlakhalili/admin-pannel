e13f934ec048b070d1a494d160f0243f23d1c217
Mahla <Mahlakhalili1@gmail.com>
2024-12-10 08:39:16 -0800

    Update FormSkeleton.jsx

--- src/skeleton/FormSkeleton.jsx
+++ src/skeleton/FormSkeleton.jsx
@@ -3,10 +3,10 @@ const FormSkeleton = () => {
 	return (
 		<div className="page">
 			<div className="skeleton w-40 h-8"></div>
-			<div className="grid grid-col-2 gap-4">
+			<div className="grid grid-cols-2 gap-4">
 				<InputSkeleton />
 				<InputSkeleton />
-				<InputSkeleton />
+				<InputSkeleton className="col-span-2" />
 				<InputSkeleton />
 				<InputSkeleton />
 				<InputSkeleton />
@@ -26,8 +26,19 @@ const InputSkeleton = ({ className = '' }) => {
 		</div>
 	);
 };
+const TeaxtareaSkeleton = ({ className = '' }) => {
+	return (
+		<div className={`grid gap-2 ${className}`}>
+			<div className="skeleton h-3 w-20"></div>
+			<div className="skeleton h-10"></div>
+		</div>
+	);
+};
 
 InputSkeleton.propTypes = {
+	className: PropTypes.string,
+};
+TeaxtareaSkeleton.propTypes = {
 	className: PropTypes.string,
 };
 
