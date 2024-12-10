26f9c399ab2686d99dc971f60bfcac5f0578634a
Mahla <Mahlakhalili1@gmail.com>
2024-12-10 04:29:57 -0800

    Update useEditProduct.js

--- src/hooks/apis/product/useEditProduct.js
+++ src/hooks/apis/product/useEditProduct.js
@@ -2,5 +2,11 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const useEditProduct = () => {};
+const useEditProduct = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        
+    })
+
+};
 export default useEditProduct;
