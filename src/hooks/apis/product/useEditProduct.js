c7da6580bf0e171ed638184307716a98251b529a
Mahla <Mahlakhalili1@gmail.com>
2024-12-10 04:27:58 -0800

    Create useEditProduct.js

---
+++ src/hooks/apis/product/useEditProduct.js
@@ -0,0 +1,6 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const useEditProduct = () => {};
+export default useEditProduct;
