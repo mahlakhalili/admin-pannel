030bb661911618a4dd0d68163a96bea4272221d7
Mahla <Mahlakhalili1@gmail.com>
2024-12-11 00:26:52 -0800

    Update useEditProduct.js

--- src/hooks/apis/product/useEditProduct.js
+++ src/hooks/apis/product/useEditProduct.js
@@ -1,12 +1,11 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import axios from 'axios';
+import apiClient from '../../../apiClient';
 import { toast } from 'react-toastify';
 
 const useEditProduct = ({ onEdit = () => {} }) => {
 	const queryClient = useQueryClient();
 	return useMutation({
-		mutationFn: (data) =>
-			axios.patch(`http://localhost:1000/product/${data.id}`).then((res) => res.data),
+		mutationFn: (data) => apiClient.patch(`/product/${data.id}`).then((res) => res.data),
 		onSuccess: () => {
 			queryClient.invalidateQueries({
 				queryKey: ['products'],
