5e5019b9d804d11badac771fa4ecdae935bf00ae
Mahla <Mahlakhalili1@gmail.com>
2024-12-23 12:25:54 -0800

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Icon from './Icon';
 import { BiLoaderAlt } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 const Button = ({
 	text,
 	onClick,
 	to,
+	icon,
 	color = '',
 	type = 'button',
 	loading = false,
 	disabled = false,
 }) => {
-	const Tag = to ? Link : 'button'
+	const Tag = to ? Link : 'button';
 	return (
 		<Tag
 			type={type}
@@ -24,17 +26,28 @@ const Button = ({
 			})}
 			to={to}
 		>
-			{loading ? <BiLoaderAlt className="animate-spin" /> : text}
+			{loading ? (
+				<Icon>
+					{' '}
+					<BiLoaderAlt />{' '}
+				</Icon>
+			) : (
+				<div className="flex items-center justify-center gap-2">
+					{Icon && <Icon>{icon}</Icon>}
+					{text && <span>{text}</span>}
+				</div>
+			)}
 		</Tag>
 	);
 };
 Button.propTypes = {
 	text: PropTypes.string,
-	to:PropTypes.string,
+	to: PropTypes.string,
 	onClick: PropTypes.func,
 	loading: PropTypes.bool,
 	disabled: PropTypes.bool,
-	color: PropTypes.oneOf(['blue', 'green', 'red' ,'outline-red']),
+	icon: PropTypes.node,
+	color: PropTypes.oneOf(['blue', 'green', 'red', 'outline-red']),
 	type: PropTypes.oneOf(['button', 'submit']),
 };
 export default Button;
