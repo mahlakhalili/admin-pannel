1a3231d2744e2576d5a0abe5a274b936bc48113b
Mahla <Mahlakhalili1@gmail.com>
2024-12-23 11:47:12 -0800

    Update EmptyBox.jsx

--- src/components/EmptyBox.jsx
+++ src/components/EmptyBox.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
-import Icon from './Icon'
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import Icon from './Icon';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
 
 const EmptyBox = ({ title, text }) => {
 	return (
 		<div className="flex flex-col justify-center items-center gap-6 py-10 bg-blue-100 rounded-lg">
+			<Icon>
+				<FaMagnifyingGlass />
+			</Icon>
 			<h2 className="text-gray-900 text-xl">{title}</h2>
 			<p className="text-gray-700 text-lg">{text}</p>
 		</div>
