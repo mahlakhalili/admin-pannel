608d168073d936b77816a3331dd711d7201f001a
Mahla <Mahlakhalili1@gmail.com>
2024-12-23 11:40:30 -0800

    Update EmptyBox.jsx

--- src/components/EmptyBox.jsx
+++ src/components/EmptyBox.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const EmptyBox = ({ title, text }) => {
 	return (
-		<div className="flex flex-col justify-center items-center gap-6 py-10">
+		<div className="flex flex-col justify-center items-center gap-6 py-10 bg-blue-100 rounded-lg">
 			<h2 className="text-gray-900 text-xl">{title}</h2>
 			<p className="text-gray-700 text-lg">{text}</p>
 		</div>
