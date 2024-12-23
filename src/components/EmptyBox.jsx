2d778ef28bf2f49943836a53dbbe2661c17d2fef
Mahla <Mahlakhalili1@gmail.com>
2024-12-23 11:22:15 -0800

    Update EmptyBox.jsx

--- src/components/EmptyBox.jsx
+++ src/components/EmptyBox.jsx
@@ -1,5 +1,10 @@
-const EmptyBox = () => {
-	return <div></div>;
+const EmptyBox = ({ title, text }) => {
+	return (
+		<div className="flex flex-col justify-center items-center gap-6 py-10">
+			<h2></h2>
+			<p></p>
+		</div>
+	);
 };
 
 export default EmptyBox;
