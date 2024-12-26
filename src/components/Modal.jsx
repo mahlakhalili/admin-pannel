6ef0b86e9b52337a40cc42d6c6282ba09dec7727
Mahla <Mahlakhalili1@gmail.com>
2024-12-26 13:10:28 -0800

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,8 +4,8 @@ import { IoCloseSharp } from 'react-icons/io5';
 
 const Modal = ({ title = '' }) => {
 	return (
-		<div className="fixed top-0 right-0 w-full h-full bg-black/80 backdrop-blur-sm z-50">
-			<div className="grid grid-rows-[max-content_1fr_max-content] bg-white ">
+		<div className="fixed flex items-center justify-center top-0 right-0 w-full h-full bg-black/80 backdrop-blur-sm z-50">
+			<div className="grid grid-rows-[max-content_1fr_max-content] bg-white rounded-lg overflow-hidden ">
 				<div className="flex items-center justify-between p-4 border-b ">
 					<h3 className="text-xl font-semibold text-gray-900 ">{title}</h3>
 					<Button
