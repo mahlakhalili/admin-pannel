b6eedeb043164c1e24bdfbee34b0b815aa8d845f
Mahla <Mahlakhalili1@gmail.com>
2024-12-26 12:41:00 -0800

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 const Modal = () => {
 	return(
-        <div className="fixed top-0 right-0 w-full h-full bg-black/80 z-50"></div>
+        <div className="fixed top-0 right-0 w-full h-full bg-black/80 backdrop-blur-sm z-50"></div>
     )
 
 };
