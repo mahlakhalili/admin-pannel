ee4762cec66b3b014dcf4403bf0c0ad68dd4009f
Mahla <Mahlakhalili1@gmail.com>
2024-12-26 12:53:52 -0800

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,9 @@
 const Modal = () => {
-	return(
-        <div className="fixed top-0 right-0 w-full h-full bg-black/80 backdrop-blur-sm z-50"></div>
-    )
-
+	return (
+		<div className="fixed top-0 right-0 w-full h-full bg-black/80 backdrop-blur-sm z-50">
+			<div className="grid grid-rows-[max-content_1fr_max-content] bg-white "></div>
+		</div>
+	);
 };
 
 export default Modal;
