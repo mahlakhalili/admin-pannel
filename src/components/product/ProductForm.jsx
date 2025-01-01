162e09117d35f87ede0adb9e908e75ff447512ea
Mahla <Mahlakhalili1@gmail.com>
2025-01-01 12:07:35 -0800

    Update ProductForm.jsx

--- src/components/product/ProductForm.jsx
+++ src/components/product/ProductForm.jsx
@@ -1,6 +1,6 @@
 import { FormProvider, useForm } from 'react-hook-form';
 import { useState } from 'react';
-import { BsCloudUpload } from "react-icons/bs";
+import { BsCloudUpload } from 'react-icons/bs';
 import PropTypes from 'prop-types';
 import Input from '../Input';
 import Button from '../Button';
@@ -74,12 +74,9 @@ const ProductForm = ({
 				/>
 
 				<div className="flex items-center justify-center w-full">
-					<label
-						htmlFor="dropzone-file"
-						className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50   hover:bg-gray-100  "
-					>
+					<div className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50   hover:bg-gray-100  ">
 						<div className="flex flex-col items-center justify-center pt-5 pb-6">
-							<BsCloudUpload/>
+							<BsCloudUpload />
 							<p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
 								Click to upload
 							</p>
@@ -87,12 +84,7 @@ const ProductForm = ({
 								SVG, PNG, JPG or GIF (MAX. 800x400px)
 							</p>
 						</div>
-						<input
-							id="dropzone-file"
-							type="file"
-							className="hidden"
-						/>
-					</label>
+					</div>
 				</div>
 
 				{/* <Input
