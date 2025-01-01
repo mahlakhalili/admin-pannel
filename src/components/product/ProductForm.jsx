cf5ca93b79cdd0c0a6f517f02632f2015f371179
Mahla <Mahlakhalili1@gmail.com>
2025-01-01 11:49:14 -0800

    Update ProductForm.jsx

--- src/components/product/ProductForm.jsx
+++ src/components/product/ProductForm.jsx
@@ -1,5 +1,6 @@
 import { FormProvider, useForm } from 'react-hook-form';
 import { useState } from 'react';
+import { BsCloudUpload } from "react-icons/bs";
 import PropTypes from 'prop-types';
 import Input from '../Input';
 import Button from '../Button';
@@ -74,13 +75,13 @@ const ProductForm = ({
 
 				<div className="flex items-center justify-center w-full">
 					<label
-						for="dropzone-file"
+						// for="dropzone-file"
 						className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50   hover:bg-gray-100  "
 					>
 						<div className="flex flex-col items-center justify-center pt-5 pb-6">
+							<BsCloudUpload/>
 							<p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-								<span className="font-semibold">Click to upload</span> or drag and
-								drop
+								Click to upload
 							</p>
 							<p className="text-xs text-gray-500 dark:text-gray-400">
 								SVG, PNG, JPG or GIF (MAX. 800x400px)
