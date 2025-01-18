cb41673cce7d49e03d174da9e05769c9cb4bf2d1
Mahla <Mahlakhalili1@gmail.com>
2025-01-18 09:12:33 -0800

    Update Select.jsx

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,7 +1,44 @@
 import PropTypes from 'prop-types';
 import { useController, useFormContext } from 'react-hook-form';
-const Select = () => {
-	return <div></div>;
+const Select = (
+	label,
+	placeholder = '',
+	name = '',
+	defaultValue = '',
+	className = '',
+	rules = {},
+	onChange = () => {}
+) => {
+	const { control } = useFormContext();
+	const { field, fieldState } = useController({
+		control,
+		defaultValue,
+		name,
+		rules,
+	});
+	return (
+		<div className={`form-input ${className}`}>
+			<label>{label}</label>
+			<Tag
+				{...field}
+				type={type === 'currency' ? 'number' : type}
+				placeholder={placeholder}
+				onChange={(e) => {
+					onChange(e.target.value, e);
+					if (field) return field.onChange(e);
+				}}
+			/>
+			{type === 'currency' && (
+				<span className="text-end text-gray-700 text-xs">
+					{numberToCurrency(+watch(name))} تومان
+				</span>
+			)}
+
+			{fieldState?.error && (
+				<span className="text-red-600 text-sm">{fieldState.error.message}</span>
+			)}
+		</div>
+	);
 };
 
 export default Select;
