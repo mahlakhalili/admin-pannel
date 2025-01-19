5a22fa235eac7555964323f1b5c617310ee5c834
Mahla <Mahlakhalili1@gmail.com>
2025-01-19 08:39:38 -0800

    Update AddCategory.jsx

--- src/pages/category/AddCategory.jsx
+++ src/pages/category/AddCategory.jsx
@@ -1,7 +1,9 @@
+import CategoryForm from '../../components/category/CategoryForm';
 const AddCategory = () => {
 	return (
 		<div className="grid gap-4">
 			<h1 className="text-2xl">افزودن دسته بندی</h1>
+			<CategoryForm />
 		</div>
 	);
 };
