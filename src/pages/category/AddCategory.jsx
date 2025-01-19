adc36e887ae4e29ac08ce10ed7f187330ff9c511
Mahla <Mahlakhalili1@gmail.com>
2025-01-19 11:28:53 -0800

    Update AddCategory.jsx

--- src/pages/category/AddCategory.jsx
+++ src/pages/category/AddCategory.jsx
@@ -1,9 +1,21 @@
 import CategoryForm from '../../components/category/CategoryForm';
+import useAddCategory from '../../hooks/apis/category/useAddCategory';
+import { useNavigate } from 'react-router-dom';
 const AddCategory = () => {
+	const navigate = useNavigate();
+	const addCategory = useAddCategory({
+		onAdd: () => navigate('/category/list'),
+	});
 	return (
 		<div className="grid gap-4">
 			<h1 className="text-2xl">افزودن دسته بندی</h1>
-			<CategoryForm />
+			<CategoryForm
+				mode="ADD"
+				isAdding={addCategory.isPending}
+				onAdd={(formData) => {
+					addCategory.mutate(formData);
+				}}
+			/>
 		</div>
 	);
 };
