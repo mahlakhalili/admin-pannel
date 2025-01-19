6bb85a13a701337cc44ecb9554a6af83d0b708e4
Mahla <Mahlakhalili1@gmail.com>
2025-01-19 07:56:59 -0800

    Update AddCategory.jsx

--- src/pages/category/AddCategory.jsx
+++ src/pages/category/AddCategory.jsx
@@ -1,5 +1,9 @@
 const AddCategory = () => {
-	return <div></div>;
+	return (
+		<div className="grid gap-4">
+			<h1 className="text-2xl">افزودن دسته بندی</h1>
+		</div>
+	);
 };
 
 export default AddCategory;
