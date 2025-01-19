303d7b7a51026fb8c3c9c7b408a70dc37ea53277
Mahla <Mahlakhalili1@gmail.com>
2025-01-19 05:18:46 -0800

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import App from './App';
 import ProductList from './pages/product/ProductList';
 import AddProduct from './pages/product/AddProduct';
 import EditProduct from './pages/product/EditProduct';
+import AddCategory from './pages/category/AddCategory';
 const Router = createBrowserRouter([
 	{
 		path: '/',
@@ -19,6 +20,10 @@ const Router = createBrowserRouter([
 			{
 				path: '/product/edit/:productId',
 				element: <EditProduct />,
+			},
+			{
+				path: '/product/add/category',
+				element: <AddCategory />,
 			},
 		],
 	},
