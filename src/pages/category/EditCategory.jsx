97cff6ead9c6b3f8a476f101248a384d8128cf18
Mahla <Mahlakhalili1@gmail.com>
2025-01-20 13:31:35 -0800

    Update EditCategory.jsx

--- src/pages/category/EditCategory.jsx
+++ src/pages/category/EditCategory.jsx
@@ -1,11 +1,17 @@
+import useEditCategory from '../../hooks/apis/category/useEditCategory';
 import useGetCategory from '../../hooks/apis/category/useGetCategory';
 import CategoryForm from '../../components/category/CategoryForm';
 import FormSkeleton from '../../skeleton/FormSkeleton';
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 const EditCategory = () => {
 	const { categoryId } = useParams();
+	const navigate = useNavigate();
 	const { data: category, isLoading } = useGetCategory(categoryId);
+	const EditCategory = useEditCategory({
+		onEdit: () => navigate('/category/list'),
+	});
 	if (isLoading) return <FormSkeleton />;
 	return (
 		<div className="grid gap-4">
@@ -13,6 +19,10 @@ const EditCategory = () => {
 			<CategoryForm
 				mode="EDIT"
 				data={category}
+				isEditing={EditCategory.isPending}
+				onEdit={(formData) => {
+					EditCategory.mutate(formData);
+				}}
 			/>
 		</div>
 	);
