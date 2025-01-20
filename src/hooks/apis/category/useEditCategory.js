6ddea91d546e71917de1120026af6a572006aac7
Mahla <Mahlakhalili1@gmail.com>
2025-01-20 13:03:30 -0800

    Create useEditCategory.js

---
+++ src/hooks/apis/category/useEditCategory.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import apiClient from '../../../apiClient';
+import { toast } from 'react-toastify';
+
+const useEditCategory = ({ onEdit = () => {} }) => {
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: (data) => apiClient.patch(`/category/${data.id}`).then((res) => res.data),
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['category'],
+			});
+			onEdit();
+			toast.success('محصول با موفقیت ویرایش شد');
+		},
+	});
+};
+export default useEditCategory;
