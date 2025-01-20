5e17d7a0bc56dbd8dfdbc420648560d2d4144e3d
Mahla <Mahlakhalili1@gmail.com>
2025-01-20 10:07:05 -0800

    Update CategoryList.jsx

--- src/pages/category/CategoryList.jsx
+++ src/pages/category/CategoryList.jsx
@@ -22,11 +22,65 @@ const CategoryList = () => {
 			{categories?.length === 0 && (
 				<EmptyBox
 					title="هیچ دسته بندی وجود ندارد."
-					text="لطفا یک دسته بندی به لیست دسته بندی اضافه کنید."
+					text="لطفا یک دسته بندی به لیست اضافه کنید."
 				/>
+			)}
+			{categories?.length > 0 && (
+				<div className="table-wrapper">
+					<table>
+						<thead>
+							<tr>
+								<th>ردیف</th>
+								<th>عنوان دسته بندی</th>
+								<th className="row">عملیات</th>
+							</tr>
+						</thead>
+						<tbody>
+							{categories?.map((category, index) => (
+								<TableRow
+									key={category.id}
+									row={index + 1}
+									// {...product}
+									id={category.id}
+									title={category.title}
+								/>
+							))}
+						</tbody>
+					</table>
+				</div>
 			)}
 		</div>
 	);
+};
+const TableRow = ({ row, id, title }) => {
+	return (
+		<tr key={id}>
+			<td>{row}</td>
+			<td>{title}</td>
+			<td>
+				<div className="btns flex items-center gap-4">
+					<Button
+						icon={<MdEdit size={18} />}
+						color="green"
+						to={`/product/edit/${id}`}
+					/>
+					<Button
+						icon={<MdDelete size={18} />}
+						color="red"
+						// loading={deleteProduct.isPending}
+						// onClick={() => {
+						// 	deleteProduct.mutate(id);
+						// }}
+					/>
+				</div>
+			</td>
+		</tr>
+	);
+};
+TableRow.propTypes = {
+	row: PropTypes.number,
+	id: PropTypes.string,
+	title: PropTypes.string,
 };
 
 export default CategoryList;
