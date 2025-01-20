528bc1eca7946b714bf8b8a31fd7e17a5c8b48b8
Mahla <Mahlakhalili1@gmail.com>
2025-01-20 12:59:51 -0800

    Update EditCategory.jsx

--- src/pages/category/EditCategory.jsx
+++ src/pages/category/EditCategory.jsx
@@ -1,10 +1,19 @@
+import useGetCategory from '../../hooks/apis/category/useGetCategory';
 import CategoryForm from '../../components/category/CategoryForm';
+import FormSkeleton from '../../skeleton/FormSkeleton';
+import { useParams } from 'react-router-dom';
 
 const EditCategory = () => {
+	const { categoryId } = useParams();
+	const { data: category, isLoading } = useGetCategory(categoryId);
+	if (isLoading) return <FormSkeleton />;
 	return (
 		<div className="grid gap-4">
 			<h1 className="text-2xl">ویرایش دسته بندی</h1>
-			<CategoryForm mode="EDIT" />
+			<CategoryForm
+				mode="EDIT"
+				data={category}
+			/>
 		</div>
 	);
 };
