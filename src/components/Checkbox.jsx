d628df46220b9e78998a5aa90f633e9ceb7537ee
Mahla <Mahlakhalili1@gmail.com>
2025-02-02 09:40:12 -0800

    Create Checkbox.jsx

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,13 @@
+import { useController, useFormContext } from 'react-hook-form';
+const Checkbox = () => {
+	const { control, watch } = useFormContext();
+	const { field, fieldState } = useController({
+		control,
+		defaultValue,
+		name,
+		rules,
+	});
+	return <div></div>;
+};
+
+export default Checkbox;
