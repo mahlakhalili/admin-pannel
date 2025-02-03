d3e8bae914c2c7b937f6682c93859ca747214fb3
Mahla <Mahlakhalili1@gmail.com>
2025-02-03 04:53:22 -0800

    Update Checkbox.jsx

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -9,15 +9,13 @@ const Checkbox = ({ label, name, defaultValue, rules, className = '' }) => {
 		rules,
 	});
 	return (
-		<div className="flex items-center gap-2">
+		<label className="flex items-center gap-2">
 			<Input
 				type="checkbox"
-				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500  focus:ring-2 "
 			/>
-			<label className=" text-sm font-medium text-gray-900 dark:text-gray-300">
-				{label}
-			</label>
-		</div>
+			<span className=" text-sm font-medium text-gray-900 ">{label}</span>
+		</label>
 	);
 };
 
