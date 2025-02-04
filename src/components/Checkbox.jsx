c68f65775074add02ba9f45d104a71f9b1f6e391
Mahla <Mahlakhalili1@gmail.com>
2025-02-04 13:21:51 -0800

    Update Checkbox.jsx

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,14 +1,14 @@
-// import { useController, useFormContext } from 'react-hook-form';
+import { useController, useFormContext } from 'react-hook-form';
 import classNames from 'classnames';
 import Input from './Input';
-const Checkbox = ({ label, name, defaultValue, rules, className = '' }) => {
-	// const { control, watch } = useFormContext();
-	// const { field, fieldState } = useController({
-	// 	control,
-	// 	defaultValue,
-	// 	name,
-	// 	rules,
-	// });
+const Checkbox = ({ label, name, defaultValue, rules, className = '', onChange = () => {} }) => {
+	const { control, watch } = useFormContext();
+	const { field, fieldState } = useController({
+		control,
+		defaultValue,
+		name,
+		rules,
+	});
 	return (
 		<label
 			className={classNames({
@@ -19,6 +19,12 @@ const Checkbox = ({ label, name, defaultValue, rules, className = '' }) => {
 			<input
 				type="checkbox"
 				className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500  focus:ring-2 "
+				{...field}
+				onChange={(e) => {
+					const isChecked = e.currentTarget.checked;
+					onChange(isChecked);
+					field.onChange(e);
+				}}
 			/>
 			<span className=" text-sm font-medium text-gray-900 ">{label}</span>
 		</label>
