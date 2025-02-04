b293b9bc57605dadd57bc4d6aa6e26615b66bcbe
Mahla <Mahlakhalili1@gmail.com>
2025-02-04 08:36:58 -0800

    Update Checkbox.jsx

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,18 +1,24 @@
-import { useController, useFormContext } from 'react-hook-form';
+// import { useController, useFormContext } from 'react-hook-form';
+import classNames from 'classnames';
 import Input from './Input';
 const Checkbox = ({ label, name, defaultValue, rules, className = '' }) => {
-	const { control, watch } = useFormContext();
-	const { field, fieldState } = useController({
-		control,
-		defaultValue,
-		name,
-		rules,
-	});
+	// const { control, watch } = useFormContext();
+	// const { field, fieldState } = useController({
+	// 	control,
+	// 	defaultValue,
+	// 	name,
+	// 	rules,
+	// });
 	return (
-		<label className="flex items-center gap-2">
-			<Input
+		<label
+			className={classNames({
+				'flex items-center  gap-2 h-max': true,
+				[className]: className,
+			})}
+		>
+			<input
 				type="checkbox"
-				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500  focus:ring-2 "
+				className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500  focus:ring-2 "
 			/>
 			<span className=" text-sm font-medium text-gray-900 ">{label}</span>
 		</label>
