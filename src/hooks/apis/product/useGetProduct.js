edf3a51ca22f5085f2ec7e92ce6e1879f8b0c7af
Mahla <Mahlakhalili1@gmail.com>
2025-02-08 04:34:27 -0800

    Update useGetProduct.js

--- src/hooks/apis/product/useGetProduct.js
+++ src/hooks/apis/product/useGetProduct.js
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import apiClient from '../../../apiClient';
 
-const useGetProduct = ({ productId }) => {
+const useGetProduct = (productId) => {
 	return useQuery({
-		queryKey: ['product', productId],
+		queryKey: ['products', productId],
 		queryFn: () => apiClient.get(`/product/${productId}`).then((res) => res.data),
 	});
 };
