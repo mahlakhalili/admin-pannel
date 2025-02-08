dea8854edd6ac13e00e4e49880f49d66b787cf6d
Mahla <Mahlakhalili1@gmail.com>
2025-02-08 04:46:48 -0800

    Update useGetCategory.js

--- src/hooks/apis/category/useGetCategory.js
+++ src/hooks/apis/category/useGetCategory.js
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import apiClient from '../../../apiClient';
 
-const useGetCategory = ({ categoryId }) => {
+const useGetCategory = ( categoryId ) => {
 	return useQuery({
 		queryKey: ['category', categoryId],
 		queryFn: () => apiClient.get(`/category/${categoryId}`).then((res) => res.data),
