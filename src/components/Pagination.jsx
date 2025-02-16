2a333adf7382030e76ec4bf89e56491b7b127104
Mahla <Mahlakhalili1@gmail.com>
2025-02-16 03:03:53 -0800

    Update Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,98 +1,19 @@
 import classNames from 'classnames';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-const Pagination = () => {
+const Pagination = ({totalPages}) => {
 	return (
-		<nav aria-label="Page navigation example">
-			<ul className="flex items-center -space-x-px h-10 text-base">
-				<li>
-					<a
-						href="#"
-						className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700  "
-					>
-						<span className="sr-only">Previous</span>
-						<svg
-							className="w-3 h-3 rtl:rotate-180"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 6 10"
-						>
-							<path
-								stroke="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M5 1 1 5l4 4"
-							/>
-						</svg>
-					</a>
-				</li>
-				<li>
-					<a
-						href="#"
-						className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700  "
-					>
-						1
-					</a>
-				</li>
-				<li>
-					<a
-						href="#"
-						className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700  "
-					>
-						2
-					</a>
-				</li>
-				<li>
-					<a
-						href="#"
-						aria-current="page"
-						className="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 "
-					>
-						3
-					</a>
-				</li>
-				<li>
-					<a
-						href="#"
-						className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
-					>
-						4
-					</a>
-				</li>
-				<li>
-					<a
-						href="#"
-						className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
-					>
-						5
-					</a>
-				</li>
-				<li>
-					<a
-						href="#"
-						className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 "
-					>
-						<span className="sr-only">Next</span>
-						<svg
-							className="w-3 h-3 rtl:rotate-180"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 6 10"
-						>
-							<path
-								stroke="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="m1 9 4-4-4-4"
-							/>
-						</svg>
-					</a>
-				</li>
+		<div aria-label="Page navigation example">
+			<ul className="flex items-center -space-x-px h-10 text-base mx-auto">
+				<PaginationBtn
+					icon={<IoIosArrowForward />}
+					className="rounded-s-lg"
+				/>
+				<PaginationBtn
+					icon={<IoIosArrowBack />}
+					className="rounded-e-lg"
+				/>
 			</ul>
-		</nav>
+		</div>
 	);
 };
 const PaginationBtn = ({
@@ -105,7 +26,15 @@ const PaginationBtn = ({
 }) => {
 	return (
 		<button
-			className={classNames({ '': true })}
+			className={classNames({
+				'flex items-center justify-center px-4 h-10 leading-tight border': true,
+				'z-10  text-blue-600  border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 ':
+					isActive,
+				' text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700':
+					!isActive,
+				'opacity-70': disabled,
+				[className]: className,
+			})}
 			onClick={onClick}
 			disabled={disabled}
 		>
