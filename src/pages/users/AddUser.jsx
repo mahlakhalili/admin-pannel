42404b5f6f1819da8140c64b2eff88c3f78ff8ff
Mahla <Mahlakhalili1@gmail.com>
2025-02-22 10:31:13 -0800

    Create AddUser.jsx

---
+++ src/pages/users/AddUser.jsx
@@ -0,0 +1,11 @@
+import UserForm from '../../components/users/UserForm';
+const AddUser = () => {
+	return (
+		<div>
+			<h1>افزودن کاربر</h1>
+			<UserForm />
+		</div>
+	);
+};
+
+export default AddUser;
