20e7c5c0c96bae9503d9893062b403ecd6f37cc8
Mahla <Mahlakhalili1@gmail.com>
2025-02-22 10:31:07 -0800

    Create UserForm.jsx

---
+++ src/components/users/UserForm.jsx
@@ -0,0 +1,46 @@
+// import PropTypes from 'prop-types';
+import { FormProvider, useForm } from 'react-hook-form';
+import Input from '../Input';
+// import Button from '../Button';
+const UserForm = () => {
+	const formMethods = useForm();
+	return (
+		<FormProvider {...formMethods}>
+			<form className="form grid gap-4">
+				<div className="userForm space-y-4">
+					<div className="card grid-cols-2">
+						<Input
+							name="firstName"
+							label="نام"
+						/>
+						<Input
+							name="lastName"
+							label=" نام خانوادگی"
+						/>
+						<Input
+							name="province"
+							label="استان"
+						/>
+						<Input
+							name="city"
+							label="شهر"
+						/>
+						<Input
+							name="phone"
+							label="شماره تلفن"
+                            type="number"
+						/>
+
+						<Input
+							name="postCode"
+							label="کد پستی"
+                            type="number"
+						/>
+					</div>
+				</div>
+			</form>
+		</FormProvider>
+	);
+};
+
+export default UserForm;
