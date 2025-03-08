ee164842565d122fc213931053272d914b87e37e
Mahla <Mahlakhalili1@gmail.com>
2025-03-08 11:35:27 -0800

    Create useAddUser.js

---
+++ src/hooks/apis/user/useAddUser.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import apiClient from '../../../apiClient';
+
+import { toast } from 'react-toastify';
+
+const useAddUser = ({ onAdd = () => {} }) => {
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: (data) => apiClient.post('/user', data).then((res) => res.data),
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['users'],
+			});
+			onAdd();
+			toast.success('کاربر با موفقیت افزوده شد');
+		},
+	});
+};
+
+export default useAddUser;
