de50f0828e26042107cb8bb62b4cad02c9d3f3fa
Mahla <Mahlakhalili1@gmail.com>
2025-03-09 04:55:16 -0700

    Update AddUser.jsx

--- src/pages/users/AddUser.jsx
+++ src/pages/users/AddUser.jsx
@@ -1,9 +1,15 @@
 import UserForm from '../../components/users/UserForm';
+import useAddUser from '../../hooks/apis/user/useAddUser';
 const AddUser = () => {
+	const addUser = useAddUser({});
 	return (
 		<div className="grid gap-4 font-normal">
 			<h1 className="text-2xl">افزودن کاربر</h1>
-			<UserForm />
+			<UserForm
+				onAdd={(formData) => {
+					addUser.mutate(formData);
+				}}
+			/>
 		</div>
 	);
 };
