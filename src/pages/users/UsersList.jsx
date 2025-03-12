c85922c31724703e1a75cc48cbdfe3ba2e428661
Mahla <Mahlakhalili1@gmail.com>
2025-03-12 14:01:26 -0700

    Update UsersList.jsx

--- src/pages/users/UsersList.jsx
+++ src/pages/users/UsersList.jsx
@@ -11,6 +11,43 @@ const UsersList = () => {
 					افزودن کاربر
 				</Link>
 			</div>
+			<>
+				<div className="table-wrapper">
+					<table>
+						<thead>
+							<tr>
+								<th>نام</th>
+								<th>نام خانوادگی</th>
+								<th> استان</th>
+								<th>شهر</th>
+								<th>شماره تلفن</th>
+								<th>کد پستی</th>
+							</tr>
+						</thead>
+						{/* <tbody>
+							{products?.data?.map((product, index) => (
+								<TableRow
+									key={product.id}
+									row={index + 1}
+									{...product}
+									categories={categories}
+									// id={product.id}
+									// title={product.title}
+									// price={product.price}
+									// discount={product.discount}
+									// count={product.count}
+									// category={product.category}
+									// status={product.status}
+								/>
+							))}
+						</tbody> */}
+					</table>
+				</div>
+				{/* <Pagination
+					totalPages={products?.pages}
+					currentPage={!products?.prev ? 1 : products?.prev + 1}
+				/> */}
+			</>
 		</div>
 	);
 };
