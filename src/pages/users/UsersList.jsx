ccf52558ffbe835ff9f8069dbbf39ead80531086
Mahla <Mahlakhalili1@gmail.com>
2025-03-12 06:51:40 -0700

    userList

--- src/pages/users/UsersList.jsx
+++ src/pages/users/UsersList.jsx
@@ -1,6 +1,18 @@
-
+import { Link } from 'react-router-dom';
 const UsersList = () => {
-	return <div>list</div>;
+	return (
+		<div className="page">
+			<div className="flex items-center justify-between">
+				<h1 className="text-2xl">لیست کاربران</h1>
+				<Link
+					className="btn blue"
+					to="/users/add"
+				>
+					افزودن کاربر
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default UsersList;
