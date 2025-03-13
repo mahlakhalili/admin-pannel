ea6bc7f15c998cb320eb8ce4829324c5572dcd8b
Mahla <Mahlakhalili1@gmail.com>
2025-03-13 11:43:26 -0700

    Update AddUser.jsx

--- src/pages/users/AddUser.jsx
+++ src/pages/users/AddUser.jsx
@@ -1,7 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import UserForm from '../../components/users/UserForm';
 import useAddUser from '../../hooks/apis/user/useAddUser';
 const AddUser = () => {
-	const addUser = useAddUser({ onAdd: () => {} });
+	const navigate = useNavigate();
+	const addUser = useAddUser({ onAdd: () => navigate('/users/list') });
 	return (
 		<div className="grid gap-4 font-normal">
 			<h1 className="text-2xl">افزودن کاربر</h1>
