96f8199c048cf47ef0770252183f65786d8b9612
Mahla <Mahlakhalili1@gmail.com>
2025-03-17 04:09:01 -0700

    Update useGetUserList.js

--- src/hooks/apis/user/useGetUserList.js
+++ src/hooks/apis/user/useGetUserList.js
@@ -4,12 +4,12 @@ import apiClient from '../../../apiClient';
 const useGetUserList = ({ params }) => {
 	return useQuery({
 		queryKey: params ? ['users', params] : ['users'],
-		queryFn: (data) =>
+		queryFn: () =>
 			apiClient
 				.get(
 					'/users',
 					{ params: { _page: params._page ?? 1, _per_page: params._per_page ?? 5 } },
-					data
+					
 				)
 				.then((res) => res.data),
 	});
